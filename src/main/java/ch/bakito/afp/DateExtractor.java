b83d99768d93bbcad65dea0f3aea0ebb24217c61
bakito <bakito@gmx.net>
2015-01-25 16:48:12 +0100

    Initial commit

---
+++ src/main/java/ch/bakito/afp/DateExtractor.java
@@ -0,0 +1,13 @@
+package ch.bakito.afp;
+
+/**
+ * Created by marc on 25.01.2015.
+ */
+public class DateExtractor {
+
+    private static final String URL ="http://www.olten.ch/de/gewerbe/umwelt/abfall/abfalldaten/welcome.php?kreis_id=19";
+
+    public static void main(String... args) {
+
+    }
+}
